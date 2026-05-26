1c4db3521d5cc179c2303ba434e164b023426a4c
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-05-26 14:01:17 +0530

    Add Leap Year determination program in C

---
+++ basics/Leapyear.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int year;
+    printf("Enter your year : ");
+    scanf("%d",&year);
+    if(year%4==0 && year%100!=0 || year%400==0){
+        printf(" This year is a leap year  ");
+    }
+    else {
+        printf("This year is not a leap year ");
+    }
+    return 0;
+}
