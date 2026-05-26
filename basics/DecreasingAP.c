76a22f45c6bc48add7a83bc23a598c53cbb25406
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-05-26 13:56:56 +0530

    Add DecreasingAP.c with AP and GP examples

---
+++ basics/DecreasingAP.c
@@ -0,0 +1,25 @@
+// Q 1 - Display this ap - 100 , 97 , 94 upto all terms which are positive 
+// #include<stdio.h>
+// int main (){
+//     int a = 100 ;
+//     for (int i=1;a>0;i++){
+//         printf("%d ",a);
+//         a = a - 3 ;
+//     }
+//     return 0 ;
+// }
+
+
+// Q 2 - Display this gp - 100 , 50 , 25 upto 'n' terms .
+// #include<stdio.h> 
+// int main (){
+//     int n ;
+//     printf("Enter the number : ");
+//     scanf("%d",&n);
+//     float  a = 100 ;
+//     for (int i=1;i<=n;i++){
+//         printf("%.2f \n",a);
+//         a = a/2; 
+//     }
+//     return 0 ;
+// }
