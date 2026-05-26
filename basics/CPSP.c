c2effde0ad73d017afa53fccd3c0d50d7b1bd31d
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-05-26 13:54:21 +0530

    Add initial implementation for Cost Price and Selling Price

--- basics/CPSP.c
+++ basics/CPSP.c
@@ -1,1 +1,36 @@
+// Cost Price and Salling Price 
+#include<stdio.h>
+int main(){
+    int SP,CP;
+    printf("Enter the Cost price : ");
+    scanf("%d",&CP);
+    printf("Enter the Selling Price : ");
+    scanf("%d",&SP);
 
+    
+    // Methord-1
+    // if(CP<SP){
+    //     printf("Profit");
+    // }
+    // if(CP>SP){
+    //     printf("Loss");
+    // }
+    // if(CP==SP){
+    //     printf("NO Profit NO loss");
+    // }
+
+
+
+
+    // Methord-2
+    // if(CP<SP){
+    //     printf("Profit");
+    // }
+    // else if(CP>SP){
+    //     printf("Loss");
+    // }
+    // else{
+    //     printf("NO Profit NO loss");
+    // }
+    return 0;
+}
