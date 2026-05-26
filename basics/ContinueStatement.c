7a550c9047d70e95c52286a71883127416873e9c
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-05-26 13:52:13 +0530

    Update ContinueStatement.c

--- basics/ContinueStatement.c
+++ basics/ContinueStatement.c
@@ -1,1 +1,23 @@
+// Q - 1 write a c program to print all the odd number from 1 to 100 .
+// #include<stdio.h>
+// int main (){
+//     for (int i=1;i<=100;i++){
+//         if (i%2==0){
+//         continue;
+//         }
+//     printf("%d ",i);
+//     }
+//     return 0 ;
+// }
 
+
+// Q - 2 write a c program to print all the even number from 1 to 100 .
+// #include<stdio.h>
+// int main (){
+//     for (int i=1;i<=100;i++){
+//         if (i%2!=0){
+//             continue;
+//         }
+//         printf("%d ",i);
+//     }
+// }
