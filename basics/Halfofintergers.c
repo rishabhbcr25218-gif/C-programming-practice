5bb488af10d9a18bae1e953cf5c74152dfe0fbac
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-05-26 13:58:32 +0530

    Add Halfofintergers.c to print decimal fractions
    This program takes a decimal input and prints the fractional part.

---
+++ basics/Halfofintergers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float x;
+    printf("Enter your Decimal value : ");
+    scanf("%f",&x);
+    int y; y = x;
+    float z = x-y;
+    printf("The value in decimals : %f",z);
+    return 0;
+}
