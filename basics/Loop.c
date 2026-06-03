db9db48a4800a14be17c6e1ff32327b802eae32b
Rishabh Mishra <275927617+rishabhbcr25218-gif@users.noreply.github.com>
2026-06-03 13:00:43 +0530

    Add Loop.c with multiple loop examples

---
+++ basics/Loop.c
@@ -0,0 +1,58 @@
+// Q1
+//     #include<stdio.h>
+// int main (){
+//     int n;
+//     printf("Enter the Number : ");
+//     scanf("%d",&n);
+//     for (int i=1;i<=n;i++){
+//         printf("Hello Rishabh\n");
+//     }
+//     return 0;
+// }
+
+// Q2
+//     #include<stdio.h>
+// int main (){
+//     for(int i=1;i<=100;i++){
+//     if(i%2!=0) printf("%d ",i);
+//     }
+//     return 0;
+// }
+
+// Q3
+//     #include<stdio.h>
+// int main (){
+//     int n,m,i;
+//     printf("Enter the table Number : ");
+//     scanf("%d",&n);
+//     printf("Enter Upto how much : ");
+//     scanf("%d",&m);
+//     for(i=1;i<=m;i++){
+//         printf("%d x %d = %d\n",n,i,n*i);
+//     }
+//     return 0;
+// }
+
+// Q4
+//     #include<stdio.h>
+// int main (){
+//     int n;
+//     printf("Enter the Number : ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=2*n-1;i=i+2){
+//         printf("%d ",i);
+//     }
+//     return 0;
+// }
+
+// Q5
+// #include<stdio.h>
+// int main (){
+//     int n;
+//     printf("Enter the Number : ");
+//     scanf("%d",&n);
+//     for(int i=4;i<=3*n+1;i=i+3){
+//         printf("%d ",i);
+//     }
+//     return 0;
+// }
